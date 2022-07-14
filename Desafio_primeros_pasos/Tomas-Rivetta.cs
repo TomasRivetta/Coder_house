4f15d3f42cd6fb2b48795bb0e7bff6460e135556
Tomas Rivetta <totorivetta@gmail.com>
2022-07-14 15:45:41 -0300

    primeros_pasos

---
+++ Desafio_primeros_pasos/Tomas-Rivetta.cs
@@ -0,0 +1,57 @@
+namespace PrimerosPasos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Usuario
+    {
+        public int id;
+
+        private string nombre;
+
+        private string apellido;
+
+        private string nombreUsuario;
+
+        private int contrasenia;
+
+        private string mail;
+    }
+
+    class Producto
+    {
+        public int id;
+
+        private string descripcion;
+
+        private double costo;
+
+        private double precioVenta;
+
+        public int stock;
+
+        private int idUsuario;
+    }
+
+    class ProductoVendido
+    {
+        public int id;
+
+        private int idProducto;
+
+        public int stock;
+
+        private int idVenta;
+    }
+
+    class Venta
+    {
+        public int id;
+
+        private string comentarios;
+    }
+}
